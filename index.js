3cd980f38d581206bf6614c63be1b1e3fe8fb75e
Lohith Bandla <lohithbandla5@gmail.com>
2025-07-31 10:14:49 +0530

    wrote a hello in main

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/problems', problemsRoutes); // Add this line
 
+
+app.get('/', (req, res) => {
+  res.send('Backend of the app hello')
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
